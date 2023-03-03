7bc6c00ca7a67b15152270495bf1b606d788aa14
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-03 16:10:47 +0530

    - dump all backend.

--- server/routes/connection.go
+++ server/routes/connection.go
@@ -1,1 +1,35 @@
 package routes
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDB := "mongodb://localhost:27017/caloriedb"
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB.")
+	return client
+}
+
+
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = (*mongo.Collection)(client.Database("caloriedb").Collection(collectionName))
+	return collection
+}
--- server/routes/entries.go
+++ server/routes/entries.go
@@ -1,32 +1,175 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/krishnalagad/golang-react-calorie-tracker/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var validate = validator.New()
+
+var Client *mongo.Client = DBinstance()
+
+var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
 
 func AddEntry(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entry models.Entry
 
+	if err := c.BindJSON(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	validationErr := validate.Struct(entry)
+	if validationErr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": validationErr.Error()})
+		log.Fatal(validationErr)
+	}
+	entry.ID = primitive.NewObjectID()
+	result, insertErr := entryCollection.InsertOne(ctx, entry)
+	if insertErr != nil {
+		msg := fmt.Sprintf("Orderd item was not yet created.")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		log.Fatal(insertErr)
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result)
 }
 
 func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entries []bson.M
+	cursor, err := entryCollection.Find(ctx, bson.M{})
 
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	if err = cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, entries)
 }
 
 func GetEntryById(c *gin.Context) {
-
+	entryId := c.Params.ByName("id")
+	ID, _ := primitive.ObjectIDFromHex(entryId)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entry bson.M
+	if err := entryCollection.FindOne(ctx, bson.M{"_id": ID}).Decode(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	defer cancel()
+	fmt.Println(entry)
+	c.JSON(http.StatusOK, entry)
 }
 
 func GetEntryByIngradient(c *gin.Context) {
+	ingredient := c.Params.ByName("id")
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entries []bson.M
 
+	cursor, err := entryCollection.Find(ctx, bson.M{"ingredients": ingredient})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	if err := cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	defer cancel()
+	fmt.Println(entries)
+
+	c.JSON(http.StatusOK, entries)
 }
 
 func UpdateEntryById(c *gin.Context) {
+	entryId := c.Params.ByName("id")
+	ID, _ := primitive.ObjectIDFromHex(entryId)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entry models.Entry
 
+	if err := c.BindJSON(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	validationErr := validate.Struct(entry)
+	if validationErr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": validationErr.Error()})
+		log.Fatal(validationErr)
+	}
+
+	result, err := entryCollection.ReplaceOne(
+		ctx,
+		bson.M{"_id": ID},
+		bson.M{
+			"dish":        entry.Dish,
+			"fat":         entry.Fat,
+			"ingredients": entry.Ingredients,
+			"calories":    entry.Calories,
+		},
+	)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result.ModifiedCount)
 }
 
 func UpdateIngradientById(c *gin.Context) {
+	entryId := c.Params.ByName("id")
+	ID, _ := primitive.ObjectIDFromHex(entryId)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+	type Ingredient struct {
+		Ingredients *string `json:"ingredients"`
+	}
+
+	var ingredient Ingredient
+
+	if err := c.BindJSON(&ingredient); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+
+	result, err := entryCollection.UpdateOne(
+		ctx,
+		bson.M{"_id": ID},
+		bson.D{{Key: "$set", Value: bson.D{{Key: "ingredients", Value: ingredient.Ingredients}}}},
+	)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result.ModifiedCount)
 }
 
 func DeleteEntryById(c *gin.Context) {
+	entriID := c.Params.ByName("id")
+	ID, _ := primitive.ObjectIDFromHex(entriID)
 
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	result, err := entryCollection.DeleteOne(ctx, bson.M{"_id": ID})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Fatal(err)
+	}
+	defer cancel()
+
+	c.JSON(http.StatusOK, result.DeletedCount)
 }
-
